2e5b6a031527ac6a3dcbe05e29b30a094b6cb945
Lauren Henry <lauren.kitson88@gmail.com>
2015-10-25 16:56:14 +0000

    Contact form works!
    Added js and php files. Now contact form sends email.

---
+++ bin/contact_me.php
@@ -0,0 +1,28 @@
+<?php
+
+if(empty($_POST['name'])   ||    
+    empty($_POST['email'])  ||
+    empty($_POST['message'])||   
+    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))    
+  {     
+       echo "No arguments Provided!";   return false;    
+  } 
+     
+  $name = $_POST['name']; 
+  $email_address = $_POST['email']; 
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];      
+ 
+ // create email body and send it    
+ $to = 'lauren.kitson88@gmail.com'; // put your email 
+ $email_subject = "Contact form submitted by:  $name"; $email_body = "You have received a new message. \n\n".                 
+                   " Here are the details:\n \nName: $name \n ". 
+                   "Subject: $subject \n ".                 
+                   "Email: $email_address\n Message \n $message";
+                    
+ $headers = "From: hello@laurenkitson.co.uk\n"; 
+ $headers .= "Reply-To: $email_address";     
+ 
+ mail($to,$email_subject,$email_body,$headers); return true;            
+
+?>
